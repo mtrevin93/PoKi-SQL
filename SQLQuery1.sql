402724a3231ad61ebc6cc1613a98dc93e20d12de
Michael Trevino <mtrevin93@gmail.com>
2021-09-02 10:05:35 -0400

    Join/order/select

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -9,3 +9,24 @@
 --STEP 3:
 --SELECT COUNT(Poem.Id) as numPoems
 --FROM Poem;
+
+--STEP 4:
+--SELECT TOP 76 Name
+--FROM Author
+--ORDER BY Author.Name;
+
+--STEP 5:
+--SELECT TOP 76 a.Name, g.name
+--FROM Author a
+--Join Grade g 
+--	ON a.GradeId = g.Id
+--ORDER BY a.Name;
+
+--STEP 6:
+--SELECT TOP 76 a.Name, g.name, gen.Name
+--FROM Author a
+--Join Grade g 
+--	ON a.GradeId = g.Id
+--JOIN Gender gen
+--	ON a.GenderId = gen.Id
+--ORDER BY a.Name;
